a624714106bf95a16311a1694bc4a75547dfb38a
YxChen98 <32979705+YxChen98@users.noreply.github.com>
2018-02-02 18:18:17 +0000

    Add files via upload

--- given templates/rmi/RMIServer.java
+++ given templates/rmi/RMIServer.java
@@ -6,8 +6,10 @@ package rmi;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RMISecurityManager;
 import java.util.Arrays;
 
 import common.*;
@@ -22,12 +24,35 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerI {
 
 	public void receiveMessage(MessageInfo msg) throws RemoteException {
 
-		// TO-DO: On receipt of first message, initialise the receive buffer
+		// completed: On receipt of first message, initialise the receive buffer   
+                totalMessages=msg.totalMessages;
+                receivedMessages=new int[totalMessages];
+                System.out.println("start receiving messages");
+             
 
-		// TO-DO: Log receipt of the message
+		// check: Log receipt of the message
+                receivedMessages[msg.messageNum]=1;
 
-		// TO-DO: If this is the last expected message, then identify
+		// check: If this is the last expected message, then identify
 		//        any missing messages
+                if(msg.messageNum==totalMessages-1){
+                    int failed=0;
+
+                    for(int i=0; i<totalMessages; i++){
+                        if(receivedMessages[i]!=1)
+                            failed++;
+                    }
+
+                    if(failed==0)
+                        System.out.println("all received");
+
+                    if(failed>0){
+                        System.out.println(failed+" messages not received");
+                        System.out.println( (totalMessages-failed) + " messages received");
+                    }
+            
+                    System.exit(0);
+              }
 
 	}
 
@@ -36,23 +61,40 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerI {
 
 		RMIServer rmis = null;
 
-		// TO-DO: Initialise Security Manager
+		// Completed: Initialise Security Manager
+                if(System.getSecurityManager()==null)
+                    System.setSecurityManager(new SecurityManager());
 
-		// TO-DO: Instantiate the server class
-
-		// TO-DO: Bind to RMI registry
+		// Check: Instantiate the server class, Bind to RMI registry
+                try{
+                    rmis=new RMIServer();
+                    rebindServer("rmi://146.169.53.226:2000/RMIServer", rmis);
+                }catch(Exception e){
+                    System.out.println("binding exception");
+                    e.printStackTrace();
+                }
 
 	}
 
 	protected static void rebindServer(String serverURL, RMIServer server) {
 
-		// TO-DO:
+		// check:
 		// Start / find the registry (hint use LocateRegistry.createRegistry(...)
 		// If we *know* the registry is running we could skip this (eg run rmiregistry in the start script)
-
-		// TO-DO:
 		// Now rebind the server to the registry (rebind replaces any existing servers bound to the serverURL)
 		// Note - Registry.rebind (as returned by createRegistry / getRegistry) does something similar but
 		// expects different things from the URL field.
+                try{
+                    LocateRegistry.createRegistry(2000);
+                    Naming.rebind(serverURL, server);
+                }catch(RemoteException e){
+                    System.out.println("failed to create registry");
+                    e.printStackTrace();
+                }catch(MalformedURLException e){
+                    System.out.println("malformed URL");
+                    e.printStackTrace();
+                }
+
+
 	}
 }
